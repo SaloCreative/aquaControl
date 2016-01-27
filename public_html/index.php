e4a7bb7d23ab50d09100d084ef120765ab1582ff
Rich Comber <rich@elementalwebdesign.co.uk>
2016-01-27 21:54:08 +0000

    Map out basic required functions and sensing

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,27 @@
 <?php include('includes/header.php'); ?>
 
+    <header id="Masthead">
+        <a href="/" title="Home">Dashboard</a>
+    </header>
+
+    <section id="primary">
+        <ul>
+            <li><a href="/">Sensing requirements</a>
+                <ul>
+                    <li><a href="/">Display Level</a></li>
+                    <li><a href="/">Sump Level</a></li>
+                    <li><a href="/">Top-up Level</a></li>
+                    <li><a href="/">Display Temperature</a></li>
+                    <li><a href="/">Sump Temperature</a></li>
+                </ul>
+            </li>
+            <li><a href="/">Controls</a>
+                <ul>
+                    <li><a href="/">Increase Return Pump Flow</a></li>
+                    <li><a href="/">Decrease Return Pump Flow</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
 
 <?php include('includes/footer.php'); ?>
