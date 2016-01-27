c163547d46ad61c477a3bc77326a1b58dcfaca7a
Rich Comber <rich@elementalwebdesign.co.uk>
2016-01-27 21:53:01 +0000

    Setup basic includes

---
+++ public_html/includes/header.php
@@ -0,0 +1,11 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<head>
+    <title>Aquarium Monitor</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link href="/css/app.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
