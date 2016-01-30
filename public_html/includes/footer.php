18e4d91f1daffcc1c3123456230e99945d7dd62c
Rich Comber <rich@elementalwebdesign.co.uk>
2016-01-30 15:21:00 +0000

    setting document charset and rought footer

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -1,2 +1,7 @@
+<footer>
+    <div class="row">
+        <p style="text-align: center; font-size: 12px">© <?= date('Y'); ?> SALO CREATIVE</p>
+    </div>
+</footer>
 </body>
 </html>
--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 <!DOCTYPE html>
 <head>
     <title>Aquarium Monitor</title>
+    <meta charset="utf-8" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link href="/css/app.min.css" rel="stylesheet" type="text/css" />
 </head>
